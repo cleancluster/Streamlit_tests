c1b80d0902a6d8923b55a4be6dd90e03bcfef9ad
cleancluster <57981650+cleancluster@users.noreply.github.com>
2023-04-17 22:13:33 +0200

    Add files via upload

--- Patentansøgninger_2023/Patent_Streamlit.py
+++ Patentansøgninger_2023/Patent_Streamlit.py
@@ -80,7 +80,7 @@ color_map = {group: f"rgb({i*50 % 255}, {i*100 % 255}, {i*150 % 255})"
 
 # Create a scatter plot with markers for each country, color-coded by group
 fig = px.scatter_geo(data, lat='Latitudes', lon='Longitudes', color='Cluster',
-                     color_discrete_map=color_scale, hover_name='country')
+                     color_discrete_map=color_map, hover_name='country')
 
 # Update the layout to set the map projection and the size
 fig.update_geos(projection_type='natural earth', showland=True, landcolor='lightgray',
