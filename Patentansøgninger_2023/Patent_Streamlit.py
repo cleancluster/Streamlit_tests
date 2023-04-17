6706513242d5203cf5c6bc90cb2a62b7674ee6a7
cleancluster <57981650+cleancluster@users.noreply.github.com>
2023-04-17 22:06:45 +0200

    Add files via upload

--- Patentansøgninger_2023/Patent_Streamlit.py
+++ Patentansøgninger_2023/Patent_Streamlit.py
@@ -75,11 +75,11 @@ st.markdown('Dette er en applikation, der har til formål at fremhæve forskelli
 data = pd.read_csv("Patentansøgninger_2023/subset_with_coords.csv")
 
 # Define the color scheme for the groups
-color_scheme = {0: 'blue', 1: 'red', 2: 'green', 3: 'yellow', 4: 'purple'}
+color_scale = px.colors.qualitative.Categorical
 
 # Create a scatter plot with markers for each country, color-coded by group
 fig = px.scatter_geo(data, lat='Latitudes', lon='Longitudes', color='Cluster',
-                     color_discrete_sequence=px.colors.qualitative.Plotly, hover_name='country')
+                     color_discrete_sequence=color_scale, hover_name='country')
 
 # Update the layout to set the map projection and the size
 fig.update_geos(projection_type='natural earth', showland=True, landcolor='lightgray',
