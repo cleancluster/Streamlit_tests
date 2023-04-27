180e9fb8eeab2a892e95863366c06f92e30628f9
s174061 <s174061@student.dtu.dk>
2023-04-27 12:41:51 +0200

    Update Patent_Streamlit.py

--- Patentansøgninger_2023/Patent_Streamlit.py
+++ Patentansøgninger_2023/Patent_Streamlit.py
@@ -107,46 +107,6 @@ def convert_dataframe(df, name_str):
     file = df.to_excel(name_str)
 
 
-# Get Lottie animation
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-# Delete (hide) a page
-def delete_page(main_script_path_str, page_name):
-
-    current_pages = get_pages(main_script_path_str)
-
-    for key, value in current_pages.items():
-        if value['page_name'] == page_name:
-            del current_pages[key]
-            break
-        else:
-            pass
-    _on_pages_changed.send()
-
-# Add (make visible) a page 
-def add_page(main_script_path_str, page_name):
-    
-    pages = get_pages(main_script_path_str)
-    main_script_path = Path(main_script_path_str)
-    pages_dir = main_script_path.parent / "pages"
-
-    script_path = [f for f in list(pages_dir.glob("*.py"))+list(main_script_path.parent.glob("*.py")) if f.name.find(page_name) != -1][0]
-    script_path_str = str(script_path.resolve())
-    pi, pn = page_icon_and_name(script_path)
-    psh = calc_md5(script_path_str)
-    pages[psh] = {
-        "page_script_hash": psh,
-        "page_name": pn,
-        "icon": pi,
-        "script_path": script_path_str,
-    }
-    _on_pages_changed.send()
-
-
 # Define function for each page
 def Patent():
     st.header("Patentapplications in the period 2011 - 2022")
