156bb21752fceae51a3413372ddfdc1a5b29b6ee
s174061 <s174061@student.dtu.dk>
2023-06-13 15:20:59 +0200

    Update streamlit_app_gsheets_private_demo.py

--- gsheets-demo/streamlit_app_gsheets_private_demo.py
+++ gsheets-demo/streamlit_app_gsheets_private_demo.py
@@ -2,11 +2,12 @@ import streamlit as st
 from google.oauth2 import service_account
 from shillelagh.backends.apsw.db import connect
 
-conn = connect(":memory:")
+connection = connect(":memory:")
+cursor = connection.cursor()
 
-# Perform SQL query on the Google Sheet.
-cursor = conn.cursor()
-
-query = """SELECT * FROM "https://docs.google.com/spreadsheets/d/1-FQwy2py4xRr1WBp9B9V7yy0pbhCTbVCEZGD4cpUvzM/edit#gid=0""""
-for row in cursor.execute(query):
+SQL = """
+SELECT *
+FROM "https://docs.google.com/spreadsheets/d/1-FQwy2py4xRr1WBp9B9V7yy0pbhCTbVCEZGD4cpUvzM/edit#gid=0"
+"""
+for row in cursor.execute(SQL):
     print(row)
