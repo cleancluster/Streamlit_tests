a1816ae3fa760b8dcc591dcb6b4464389f84b3c8
s174061 <s174061@student.dtu.dk>
2023-06-13 15:19:47 +0200

    Update streamlit_app_gsheets_private_demo.py

--- gsheets-demo/streamlit_app_gsheets_private_demo.py
+++ gsheets-demo/streamlit_app_gsheets_private_demo.py
@@ -2,19 +2,11 @@ import streamlit as st
 from google.oauth2 import service_account
 from shillelagh.backends.apsw.db import connect
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
 conn = connect(":memory:")
 
 # Perform SQL query on the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
 cursor = conn.cursor()
 
-query = "SELECT * FROM a_table"
+query = """SELECT * FROM "https://docs.google.com/spreadsheets/d/1-FQwy2py4xRr1WBp9B9V7yy0pbhCTbVCEZGD4cpUvzM/edit#gid=0""""
 for row in cursor.execute(query):
     print(row)
